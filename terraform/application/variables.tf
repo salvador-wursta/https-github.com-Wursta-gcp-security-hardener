c57a64e263189a55bf93ff5c0edf2ce8314449d0
Salvador Menjivar <contact@salvadormenjivar.com>
2026-03-05 19:52:26 -0600

    fix: switch IAP domain to 34.8.226.12.nip.io magic domain

--- terraform/application/variables.tf
+++ terraform/application/variables.tf
@@ -23,7 +23,7 @@ variable "backend_image" {
 variable "iap_domain" {
   description = "The domain name for the Load Balancer (e.g., demo.wursta.com)"
   type        = string
-  default     = "demo.wursta.com"
+  default     = "34.8.226.12.nip.io"
 }
 
 variable "authorized_domain" {
